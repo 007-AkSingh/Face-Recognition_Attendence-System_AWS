e0827c5adb8a46b64b73de25c989bc92efb9d8c8
Aniket Singh <67497338+007-AkSingh@users.noreply.github.com>
2025-07-21 10:06:43 +0530

    Add files via upload

---
+++ StudentAttendence.py
@@ -0,0 +1,84 @@
+import boto3
+import requests
+import datetime
+import time
+import cv2
+
+
+#Credentials----------------------------------------------------------------------------------
+
+
+
+
+#Capture images for every 1 hour and store the image with current date and time -----------------------------------------------------------------------------------
+for j in range(0, 6):
+    current_time = datetime.datetime.now().strftime("%d-%m-%y  %H-%M-%S ")
+    print(current_time)
+    camera = cv2.VideoCapture(0)
+    
+
+        
+    while True:
+        # Capture the video frame by frame
+        ret, frame = camera.read()
+
+        # Display the resulting frame
+        cv2.imshow('frame', frame)
+        # Check if the image needs to be captured
+        if cv2.waitKey(1) & 0xFF == ord(' '):
+            # Save the captured frame as an image
+            cv2.imwrite('img/' + current_time + '.jpg', frame)
+            print("Image captured!")
+            # Reset the flag
+            break
+
+        # Check if the 'q' button is pressed to quit
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            exit()
+
+   
+    del (camera) 
+
+#Send the captured image to AWS S3 Bucket--------------------------------------------------------------------------------------
+   
+    clients3.upload_file("img/" + current_time + '.jpg', 'attendance-management-system', current_time + '.jpg')
+
+
+    #Recoginze students in captured image ---------------------------------------------------------------------------------------
+    image_path = 'img/' + current_time + '.jpg'
+    with open(image_path,'rb') as source_image:
+        source_bytes = source_image.read()
+    print(type(source_bytes))
+
+    print("Recognition Service")
+    response = client.detect_custom_labels(
+    
+    #Update the Rokognition ARN with yours   
+        
+        ProjectVersionArn='arn:aws:rekognition:ap-south1:413313710573:project/AttendanceManagementSystem/version/AttendanceManagementSystem.2023-11-05T18.37.57/1699189678901',
+
+
+        Image={
+            'Bytes': source_bytes
+        },
+
+    )
+
+    print(response)
+    if not len(response['CustomLabels']):
+         print('Not identified')
+
+    else:
+        str = response['CustomLabels'][0]['Name']
+        print(str)
+        
+        # Update the attendance of recognized student in DynamoDB by calling the API
+
+        url = "https://a8upzltoh1.execute-api.ap-south1.amazonaws.com/default/AttendFunction" + str
+        
+        resp = requests.get(url)
+        print("Attendence Mark Sucesssful")
+        if resp.status_code==200:
+            print("Success")
+
+    time.sleep(3600)
